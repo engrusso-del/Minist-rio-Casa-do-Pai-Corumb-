0e5821dc9699eb8e65c70d365cf11d801e2e05d9
CasadoPai <engrusso@gmail.com>
2026-03-04 16:08:27 -0400

    teste

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import Supabase from "@Supabase/vite-plugin"
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  logLevel: 'error', // Suppress warnings, only show errors
+  plugins: [
+    base44({
+      // Support for legacy code that imports the Supabase SDK with @/integrations, @/entities, etc.
+      // can be removed if the code has been updated to use the new SDK imports from @Supabase/sdk
+      legacySDKImports: process.env.BASE44_LEGACY_SDK_IMPORTS === 'true',
+      hmrNotifier: true,
+      navigationNotifier: true,
+      visualEditAgent: true
+    }),
+    react(),
+  ]
+});
